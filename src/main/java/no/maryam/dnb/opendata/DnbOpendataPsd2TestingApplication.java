ef07bcc90cad256edb234667c802e4508884ae6f
Maryam <maryam.aslam@outlook.com>
2019-06-14 19:21:35 +0200

    Starting spring-boot project

---
+++ src/main/java/no/maryam/dnb/opendata/DnbOpendataPsd2TestingApplication.java
@@ -0,0 +1,13 @@
+package no.maryam.dnb.opendata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnbOpendataPsd2TestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnbOpendataPsd2TestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/maryam/dnb/opendata/DnbOpendataPsd2TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package no.maryam.dnb.opendata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnbOpendataPsd2TestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
